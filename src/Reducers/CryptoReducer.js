26b3602092c9fd265f83fbbc46a5ab1d8ba21031
Steve <sstpierre2@partners.org>
2017-12-13 09:31:50 -0500

    Fixed some redux business

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -5,7 +5,7 @@ import {
 } from '../utils/ActionTypes';
 
 const initialState = {
-  isFetching: false,
+  isFetching: null,
   data: [],
   hasError: false,
   errorMessage: null
@@ -13,8 +13,9 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+
     case FETCHING_COIN_DATA:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: true,
         data: [],
         hasError: false,
@@ -22,19 +23,19 @@ export default function (state = initialState, action) {
       });
 
     case FETCHING_COIN_DATA_SUCCESS:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
         data: action.payload,
         hasError: false,
         errorMessage: null
       });
-      
+
     case FETCHING_COIN_DATA_FAIL:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
-        data: [],
+        data: action.payload,
         hasError: true,
-        errorMessage: action.payload
+        errorMessage: action.err
       });
 
     default:
--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -29,8 +29,8 @@ const CoinCard = ({ symbol, coin_name, price_usd, percent_change_24h, percent_ch
         source={{ uri: images[symbol] }}
       />
       <Text style={white}>{symbol}</Text>
-      <Text style={white}>{coin_name} <Text style={bold}>$</Text></Text>
-      <Text style={white}>{price_usd}</Text>
+      <Text style={white}>{coin_name}</Text>
+      <Text style={white}>{price_usd} <Text style={bold}>$</Text></Text>
       <Text style={white}>Change past 24 hours: {percent_change_24h}</Text>
       <Text style={white}>Change past 7 days: {percent_change_7d}</Text>
     </View>
